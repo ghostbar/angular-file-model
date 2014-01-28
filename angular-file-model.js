0146110a7f99e238862a11f8112a4ee4c63df8f4
Jose Luis Rivas <me@ghostbar.co>
2014-01-28 03:25:34 -0430

    little refactor to make it work

--- angular-file-model.js
+++ angular-file-model.js
@@ -8,23 +8,25 @@
 (function () {
   'use strict';
 
-  angular.module('angular-file-model')
-    .directive('fileModel', [
-      '$parse', 
-      function ($parse) {
+  angular.module('angular-file-model', [])
+
+  .directive('fileModel', [
+    '$parse',
+    function ($parse) {
       return {
-          restrict: 'A',
-          link: function(scope, element, attrs) {
-              var model = $parse(attrs.fileModel);
-              var modelSetter = model.assign;
-              
-              element.bind('change', function(){
-                  scope.$apply(function(){
-                      modelSetter(scope, element[0].files[0]);
-                  });
-              });
-          }
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+          var model = $parse(attrs.fileModel);
+          var modelSetter = model.assign;
+            
+          element.bind('change', function(){
+            scope.$apply(function(){
+              modelSetter(scope, element[0].files[0]);
+            });
+          });
+        }
       };
-  }]);
+    }
+  ]);
 
 })();
